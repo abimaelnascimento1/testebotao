d10e29b5c30461c76d0458ca239bef2a017b38b9
abimaelnascimento1 <santtosabimael@gmail.com>
2024-10-29 22:17:41 -0300

    adicao de um novo evento de duplo clique no botao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 document.getElementById('btn').addEventListener('click', ()=>{
     document.body.style.backgroundColor='blue';
-}
-)
+})
+
+document.getElementById('btn').addEventListener('dblclick', ()=>{
+    document.body.style.backgroundColor='white';
+})
